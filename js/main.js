58331a4fb542541daa9b0b2297c22916aa960c88
natasha-gri <natashagri.dw@gmail.com>
2022-06-10 15:25:31 +0500

    работа с файлами, порядок

--- js/main.js
+++ js/main.js
@@ -5,28 +5,25 @@ let logo = document.getElementById('logo').classList;
 let logo_nohide = "logo--nohide";
 
 function getSize() {
-    let w = window.innerWidth;
-    return w > 991
+  let w = window.innerWidth;
+  return w > 991
 };
 
 function getClassScroll() {
-  alert('hi');
-    window.addEventListener('scroll', e => {
-        if(pageYOffset > 250) {
-            navbarScroll.add(acrive_class);
-            logo.add(logo_nohide);
-        }
-        
-        else {
-          navbarScroll.remove(acrive_class);
-          logo.remove(logo_nohide);
-        }
-        
-    })
+  window.addEventListener('scroll', e => {
+    if (pageYOffset > 250) {
+      navbarScroll.add(acrive_class);
+      logo.add(logo_nohide);
+    } else {
+      navbarScroll.remove(acrive_class);
+      logo.remove(logo_nohide);
+    }
+
+  })
 };
 
 
 
 if (getSize()) {
-    getClassScroll();
+  getClassScroll();
 }
